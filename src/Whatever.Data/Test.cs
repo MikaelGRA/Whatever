7930686ab481a7f3af4e08425a3cb06ee93144e4
MikaelGRA <mikaelgra@gmail.com>
2016-11-22 17:48:54 +0100

    intial commit

---
+++ src/Whatever.Data/Test.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Whatever.Models
+{
+   public class Test
+   {
+      public long Id { get; set; }
+   }
+}
